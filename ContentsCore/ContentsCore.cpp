933719f052ee309b570a011db6729efc0ce6ee12
Github <usm9401@gmail.com>
2024-03-26 15:30:11 +0900

    엔진

---
+++ ContentsCore/ContentsCore.cpp
@@ -0,0 +1,14 @@
+#include "ContentsCore.h"
+
+ContentsCore::ContentsCore() 
+{
+}
+
+ContentsCore::~ContentsCore() 
+{
+}
+
+void ContentsCore::Initialize()
+{
+}
+
---
+++ ContentsCore/ContentsCore.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <EngineCore/EngineCore.h>
+// Ό³Έν :
+class ContentsCore : public UserCore
+{
+public:
+	// constrcuter destructer
+	ContentsCore();
+	~ContentsCore();
+
+	// delete Function
+	ContentsCore(const ContentsCore& _Other) = delete;
+	ContentsCore(ContentsCore&& _Other) noexcept = delete;
+	ContentsCore& operator=(const ContentsCore& _Other) = delete;
+	ContentsCore& operator=(ContentsCore&& _Other) noexcept = delete;
+
+protected:
+	void Initialize() override;
+private:
+
+};
+
